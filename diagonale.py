a63346982cfb4e8ee7f60b80c2005dddc3c22756
sramial <sariaka.ramialison@gmail.com>
2023-04-06 15:37:40 +0200

    word_searches

--- diagonale.py
+++ diagonale.py
@@ -1,58 +1,114 @@
 # je crée la liste des diagonales lues de gauche à droite
-def diagonales_GD(grid):
 
-    liste = [[grid[i][i] for i in range(len(grid))]]
-    for k in range(1, len(grid)):
+# fonction qui liste les diagonales inférieures
+# d'une matrice carrée de taille n (sans la diagonale principale)
 
-        # je crée la liste des diagonales inférieures, cas où n=p
+def grid_inf(grid, n):                      # good!
+    liste = []
+    for k in range(1, n):
         ligne = []
-        for j in range(len(grid) - k):
+        for j in range(n-k):
             ligne.append(grid[j+k][j])
         liste.append(ligne)
+    return liste
 
-        # je crée la liste des diagonales supérieures, cas où n=p
+
+# fonction qui liste les diagonales inférieures
+# d'une matrice carrée de taille n (sans la diagonale principale)
+
+def grid_sup(grid, n):              # very good!
+    liste = []
+    for k in range(1, n):
         ligne = []
-        for i in range(len(grid) - k):
+        for i in range(n-k):
             ligne.append(grid[i][i+k])
         liste.append(ligne)
-
     return liste
+
+
+def diagonales_GD(grid):            # excellent!
+
+    n = len(grid)
+    p = len(grid[0])
+    d = abs(n-p)
+
+    liste = [[grid[i][i] for i in range(min(n, p))]]
+
+    # je crée la liste des diagonales, cas où n ≤= p
+
+    if len(grid) < len(grid[0]):
+
+        # je crée la liste des diagonales inférieures
+        # similaire au cas où n=p
+
+        liste += grid_inf(grid, n)
+
+        # je crée la liste des diagonales supérieures dans ce cas
+
+        # d'abord la bande centrale
+        for k in range(1, d+1):
+            ligne = []
+            for i in range(n):
+                ligne.append(grid[i][i+k])
+            liste.append(ligne)
+
+        # puis les diagonales supérieures
+        new_grid = list(range(n))
+        for i in range(n):
+            new_grid[i] = grid[i][d:]
+        liste += grid_sup(new_grid, n)
+
+        return liste
+
+    if len(grid) >= len(grid[0]):                # n > p
+
+        # je crée la liste des diagonales supérieures dans ce cas
+        # même cas que n = p
+
+        liste += grid_sup(grid, p)
+
+        # je crée la liste des diagonales inférieures
+        # d'abord la bande centrale
+        for k in range(1, d+1):
+            ligne = []
+            for j in range(p):
+                ligne.append(grid[j+k][j])
+            liste.append(ligne)
+
+        # puis je rajoute les diagonales inférieures
+        new_grid = list(range(p))
+        for i in range(p):
+            new_grid[i] = grid[i+d]
+        liste += grid_inf(new_grid, n)
+
+        return liste
 
 
 # je crée la liste des diagonales lues de droite à gauche
 
 def diagonales_DG(grid):
-
-    liste = [[grid[i][-i-1] for i in range(len(grid))]]
-    for k in range(1, len(grid)):
-
-        # je crée la liste des diagonales inférieures, cas où n=p
-        ligne = []
-        for i in range(k, len(grid)):
-            ligne.append(grid[i][-i-1+k])
-        liste.append(ligne)
-
-        # je crée la liste des diagonales supérieures, cas où n=p
-        ligne = []
-        for i in range(len(grid)-k):
-            ligne.append(grid[i][-i-1-k])
-        liste.append(ligne)
-
-    return liste
+    # je crée la grid miroir
+    grid_mir = [i[::-1] for i in grid]
+    return diagonales_GD(grid_mir)
 
 
 if __name__ == '__main__':
 
     liste = open('words.dic').read()
     mots = liste.split('\n')
+    mots.pop()
     # print(mots)
 
     grille = open('grid').read()
     rows = grille.split('\n')
+    rows.pop()
     grid = []
     for row in rows:
         ligne = []
         for lettre in row:
             ligne.append(lettre)
         grid.append(ligne)
+
     print(diagonales_DG(grid))
+
+    # print(diagonales_GD(grid))
